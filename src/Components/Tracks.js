71b8dfebe7f50e2d2d48545ee2b2a5b55be0a5e1
Alisson Ross <alisson@alisson.me>
2023-02-13 19:17:15 -0800

    Many styling changes: removed borders, fixed fonts

--- src/Components/Tracks.js
+++ src/Components/Tracks.js
@@ -9,21 +9,21 @@ function Tracks() {
               <img className='Track-Logo' src={Support} alt="Support logo"/>
               <h1 className="Track-Name">Coming soon</h1>
                 <div className='Track-Text'>
-                  Stay tuned!
+                Stay tuned to learn more about our prize tracks for this year.
                 </div>
               </Card>
               <Card style={{borderRadius: '30px'}} className='Track-Card'>
               <img className='Track-Logo' src={Support} alt="Support logo"/>
               <h1 className="Track-Name">Coming soon</h1>
                 <div className='Track-Text'>
-                Stay Tuned!
+                Stay tuned to learn more about our prize tracks for this year.
                 </div>
               </Card>
               <Card style={{borderRadius: '30px'}} className='Track-Card'>
               <img className='Track-Logo' src={Support} alt="Support logo"/>
               <h1 className="Track-Name">Coming soon</h1>
                 <div className='Track-Text'>
-                Stay Tuned!
+                Stay tuned to learn more about our prize tracks for this year.
                 </div>
               </Card>
            </div>
